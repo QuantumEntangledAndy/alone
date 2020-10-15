e9a35d14ad06c508e4dec5b6c5bbc99253d8bbbd
QuantumEntangledAndy <sheepchaan@gmail.com>
2020-10-15 17:30:40 +0700

    Improvements to telegram

--- src/telegram.rs
+++ src/telegram.rs
@@ -8,6 +8,8 @@ use scopeguard::defer_on_unwind;
 use bus::{Bus, BusReader};
 use telegram_bot::{Api, UpdateKind, UserId, Integer, MessageChat, MessageKind, InputFileUpload, CanReplySendMessage, CanReplySendPhoto, Error as TeleError};
 
+use log::*;
+
 use crate::RX_TIMEOUT;
 
 pub async fn start_telegram(
@@ -23,8 +25,14 @@ pub async fn start_telegram(
     while ! ready_count.all_ready() && status.is_alive()  {
         std::thread::sleep(std::time::Duration::from_millis(500));
     }
+    if ! status.is_alive() {
+        return Ok(()); // Early exit
+    }
+    info!("Telegram Starting");
 
     let api = Api::new(token);
+
+    info!("Telegram Started");
 
     // Fetch new updates via long poll method
     let mut stream = api.stream();
@@ -36,21 +44,22 @@ pub async fn start_telegram(
                 if user.id == UserId::new(id as Integer) {
                     if let MessageKind::Text { ref data, .. } = message.kind {
                         // Print received text message to stdout.
-                        println!("<{}>: {}", &message.from.first_name, data);
-                        {
-                            send_to_bot.broadcast(data.to_string());
-                        }
-
-                        while status.is_alive() {
-                            if let Ok(reply) = get_from_bot.recv_timeout(RX_TIMEOUT) {
-                                api.send(message.text_reply(reply)).await?;
+                        if ! data.trim().starts_with('/') {
+                            {
+                                send_to_bot.broadcast(data.to_string());
                             }
-                        }
-                        while status.is_alive() {
-                            if let Ok(image_path) = get_picture_from_bot.recv_timeout(RX_TIMEOUT) {
-                                if let Some(image_path) = image_path {
-                                    if let Ok(image_path_str) = image_path.into_os_string().into_string() {
-                                        api.send(message.photo_reply(InputFileUpload::with_path(image_path_str))).await?;
+
+                            while status.is_alive() {
+                                if let Ok(reply) = get_from_bot.recv_timeout(RX_TIMEOUT) {
+                                    api.send(message.text_reply(reply)).await?;
+                                }
+                            }
+                            while status.is_alive() {
+                                if let Ok(image_path) = get_picture_from_bot.recv_timeout(RX_TIMEOUT) {
+                                    if let Some(image_path) = image_path {
+                                        if let Ok(image_path_str) = image_path.into_os_string().into_string() {
+                                            api.send(message.photo_reply(InputFileUpload::with_path(image_path_str))).await?;
+                                        }
                                     }
                                 }
                             }
